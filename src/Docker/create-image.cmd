4044ca40523f6cd979313ff144a719a73fb3f1fa
leoleopon <safari2002@list.ru>
2022-12-01 15:37:52 +0500

    Add docker clickhouse-db- confuguration.

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,5 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
 
+docker build --no-cache -f SQL\Dockerfile.ClickHouse -t nifisample/clickhouse ../SQL
+
 docker build --no-cache -f Dockerfile -t nifisample/app ../..
